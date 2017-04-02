65ca09158a3d0219d1b63d0f1fa294e639125ea0
Juan Fernando Rivas Castillo <juanfernandorc@gmail.com>
2017-04-02 11:26:40 +0000

    new version

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var dataStore = require("nedb");
+var path = require("path");
+var dbFileName = path.join(__dirname, "projects.json");
+
+var db = new dataStore({filename: dbFileName,autoload: true});
+
 var port = (process.env.PORT || 3000);
 var app = express();
-
 var baseAPI = "/api/v1";
 
-var projects = [{id:"1", titulo:"Proyecto 1", resumen:"Resumen proyecto 1", objetivo:"Objetivo proyecto 1", universidad:"Universidad de Sevilla", grupo:"G11", investigador:"Pepe Sanchez", presupuesto:"11000"},
-                {id:"2", titulo:"Proyecto 2", resumen:"Resumen proyecto 2", objetivo:"Objetivo proyecto 2", universidad:"Universidad de Cadiz", grupo:"G22", investigador:"Antonio Ramirez", presupuesto:"12000"}
-];
+//var projects = [];
 
 app.use(bodyParser.json());
 
 //Projects
 
 app.get(baseAPI + "/projects", (request,response) => {
-    response.send(projects);
+    //var projects;
+    db.find({}, (err, projects) => {
+        response.send(projects);
+    });
     console.log("GET projects");
     });
     
 app.post(baseAPI + "/projects", (request,response) => {
     var project = request.body;
-    projects.push(project);
+    //projects.push(project);
+    db.insert(project);
     response.sendStatus(201);
     console.log("POST /projects");
     });
