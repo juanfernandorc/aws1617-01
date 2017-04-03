ca4dbf5c9862e2299941aff4e4fc6fc2a87035df
Juan Fernando Rivas Castillo <juanfernandorc@gmail.com>
2017-04-03 07:32:57 +0000

    server with gui

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -8,10 +8,42 @@ angular.module("ProjectListApp").controller("ListCtrl", function($scope,$http) {
     
     $scope.addProject = function () {
       console.log($scope.newProject);  
-      $http.post("/api/v1/projects",$scope.newProject).then(function (){
+      $http.put("/api/v1/projects/" + $scope.newProject.id, $scope.newProject).then(
+    function (response) {
+    // This function handles success
+    refresh();
+    }, 
+    function (response) {
+    // this function handles error
+         $http.post("/api/v1/projects",$scope.newProject).then(function ()
+         {
+            refresh();
+            }
+            );
+    });
+      
+    };
+    
+    $scope.deleteProjects = function () {
+      $http.delete("/api/v1/projects").then(function (){
         refresh();
         });
     };
+    
+    
+    $scope.deleteProject = function () 
+    {
+        $http.delete("/api/v1/projects/" + $scope.newProject.id, $scope.newProject).then(
+            function (response) {
+                // This function handles success
+                refresh();
+            }, 
+            function (response) {
+                // this function handles error
+                 refresh();
+            });
+    };
+    
     
     refresh();
 });
