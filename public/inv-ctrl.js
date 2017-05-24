510a6e52b8920cd51efdb79cfd194936c38c37fa
Juan Fernando Rivas Castillo <juanfernandorc@gmail.com>
2017-05-24 10:20:14 +0000

    v10

--- public/inv-ctrl.js
+++ public/inv-ctrl.js
@@ -12,7 +12,7 @@ angular.module("DatosInv").controller("InvData", function($scope, $http) {
         console.log("2");
         $http.get("https://api.elsevier.com/content/search/author?apikey=3f7cfe68209df70691bbc22bd225b8cf&query=ORCID(" + $scope.searchOrcid + ")")
         .then(function(response) {
-            console.log("3");
+            console.log("3: " + response.data);
             $scope.invdata = response.data;
         }).catch("Error");
     }
