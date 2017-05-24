7f4d2414ede1f3df1461d5cc307983226ea009b2
Juan Fernando Rivas Castillo <juanfernandorc@gmail.com>
2017-05-24 11:44:33 +0000

    v13

--- public/inv-ctrl.js
+++ public/inv-ctrl.js
@@ -10,10 +10,10 @@ angular.module("DatosInv").controller("InvData", function($scope, $http) {
         console.log("1");
         $scope.invdata = [];
         console.log("2");
-        var apikeyLocal = "e94d752350ca7032b05b3fd44903fd2d";
-        var apikeyHeroku = "3f7cfe68209df70691bbc22bd225b8cf";
-//heroku        $http.get("https://api.elsevier.com/content/search/author?apikey=" + apikeyHeroku + "&httpaccept=application/json&query=ORCID(" + $scope.searchOrcid + ")")
-        $http.get("https://api.elsevier.com/content/search/author?apikey=" + apikeyLocal + "&query=ORCID(" + $scope.searchOrcid + ")")
+        // var apikey = "e94d752350ca7032b05b3fd44903fd2d";  // Local
+        var apikey = "3f7cfe68209df70691bbc22bd225b8cf"; // Heroku
+//heroku        $http.get("https://api.elsevier.com/content/search/author?apikey=" + apikey + "&httpaccept=application/json&query=ORCID(" + $scope.searchOrcid + ")")
+        $http.get("https://api.elsevier.com/content/search/author?apikey=" + apikey + "&query=ORCID(" + $scope.searchOrcid + ")")
         .then(function(response) {
             console.log(JSON.stringify(response.data, null, 4));
             //console.log(JSON.parse(response.data, null));
