a4252e4476f836f4dc55d1b334ee421d1eeea983
Juan Fernando Rivas Castillo <juanfernandorc@gmail.com>
2017-05-24 08:47:24 +0000

    v8

--- public/inv-ctrl.js
+++ public/inv-ctrl.js
@@ -1,16 +1,20 @@
 angular.module("DatosInv").controller("InvData", function($scope, $http) {
     
-        
+    /* 
     function refresh() {  
        getInvData();
-    }
     
-    function getInvData() {
+    */
+    
+    $scope.getInvData = function(){
+        console.log("1");
         $scope.invdata = [];
-        $http.get("http://api.elsevier.com/content/search/author?apikey=3f7cfe68209df70691bbc22bd225b8cf&query=ORCID(" + "0000-0002-8763-0819" + ")")
+        console.log("2");
+        $http.get("https://api.elsevier.com/content/search/author?apikey=3f7cfe68209df70691bbc22bd225b8cf&query=ORCID(" + $scope.searchOrcid + ")")
         .then(function(response) {
+            console.log("3");
             $scope.invdata = response.data;
         }).catch("Error");
     }
-    refresh();
+    //refresh();
 });
